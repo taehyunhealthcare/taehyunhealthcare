5924683e1bcc1f917948c6f25041d65bfc1a447c
taehyunhealthcare <jmy175375@naver.com>
2025-09-19 16:24:00 +0900

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -1,14 +1,63 @@
-
-// 슬라이드 페이드인 페이드 아웃
-
 $(document).ready(function () {
-  $('#slide a:gt(0)').hide();
-  setInterval(function () {
-    $('#slide a:first')
-      .fadeOut(1000)
-      .next('a')
-      .fadeIn(1000)
-      .end()
-      .appendTo('#slide');
-  }, 3000);
+  var $slides = $('#slide a');
+  var $indicators = $('#slide .indicators span');
+  var current = 0;
+  var slideCount = $slides.length;
+  var interval;
+  var fadeTime = 1000;
+
+  // 초기 상태
+  $slides.hide().eq(current).show();
+  $indicators.eq(current).addClass('active');
+
+  function showSlide(index) {
+    if (current === index) return;
+    $slides.eq(current).fadeOut(fadeTime);
+    $slides.eq(index).fadeIn(fadeTime);
+    $indicators.removeClass('active').eq(index).addClass('active');
+    current = index;
+  }
+
+  function nextSlide() {
+    var next = (current + 1) % slideCount;
+    showSlide(next);
+  }
+
+  function prevSlide() {
+    var prev = (current - 1 + slideCount) % slideCount;
+    showSlide(prev);
+  }
+
+  // 자동 슬라이드
+  function startSlider() {
+    interval = setInterval(nextSlide, 3000);
+  }
+
+  function stopSlider() {
+    clearInterval(interval);
+  }
+
+  // 버튼 이벤트
+  $('.next').click(function(){
+    stopSlider();
+    nextSlide();
+    startSlider();
+  });
+
+  $('.prev').click(function(){
+    stopSlider();
+    prevSlide();
+    startSlider();
+  });
+
+  // 인디케이터 클릭 이벤트
+  $indicators.click(function(){
+    stopSlider();
+    var index = $(this).data('index');
+    showSlide(index);
+    startSlider();
+  });
+
+  // 시작
+  startSlider();
 });
