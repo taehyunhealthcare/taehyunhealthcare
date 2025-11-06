192938d96c73b0ab0fed500a2d17a5be4b475c5b
taehyunhealthcare <jmy175375@naver.com>
2025-11-06 10:07:55 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,64 +1,60 @@
+$(document).ready(function() {
+    let maxHeight = 0;
 
-// 메뉴 스크립트
 
-$(document).ready(function () {
-  // 메뉴 호버 시 서브메뉴 표시
-  $('.menu > li').mouseover(function () {
-    $('.submenu', this).stop().slideDown(100);
-  });
-  $('.menu > li').mouseout(function () {
-    $('.submenu', this).stop().slideUp(100);
-  });
+    $(".submenu").each(function() {
+        $(this).css({
+            'display': 'block', 
+            'position': 'absolute', 
+            'visibility': 'hidden' 
+        });
+        
+        const currentHeight = $(this).outerHeight(); 
+        if (currentHeight > maxHeight) {
+            maxHeight = currentHeight;
+        }
+        
+        $(this).css({
+            'display': 'none', 
+            'position': 'absolute', 
+            'visibility': 'visible' 
+        });
+    });
 
-  // 아이콘 클릭 시 기본 동작 방지 (스크롤 방지용)
-  document.getElementById('fixedIcon')?.addEventListener('click', function (event) {
-    event.preventDefault();
-  });
+    const targetBgHeight = maxHeight + 10;
+
+    $(".menu-pc").hover(
+        function() {
+            $(".submenu").stop(true, true).slideDown(250);
+            $(".menu-bg").stop(true, true).animate({
+                height: targetBgHeight + 'px'
+            }, 250);
+        },
+        function() {
+            $(".submenu").stop(true, true).slideUp(200);
+            $(".menu-bg").stop(true, true).animate({
+                height: '0'
+            }, 200);
+        }
+    );
 });
 
+    // app.post('/submit', (req, res) => {
+    // const { title, writer, content } = req.body;
+    // const date = new Date();
 
+    
+    // db.collection('inquiries').insertOne({
+    //     title, writer, content, date
+    // });
 
+    // res.redirect('/inquiries'); 
+    // });
 
-
-// 커뮤니티 스크립트 (마우스 호버시 기존에 있던 이미지는 사라지고 텍스트가 나오는 스크립트)
-
-$(document).ready(function () {
-  $('.user').mouseover(function () {
-    $('.user > p').show();
-    $('.user > img').hide();
-  });
-  $('.user').mouseout(function () {
-    $('.user > p').hide();
-    $('.user > img').show();
-  });
-
-  $('.talk').mouseover(function () {
-    $('.talk > p').show();
-    $('.talk > img').hide();
-  });
-  $('.talk').mouseout(function () {
-    $('.talk > p').hide();
-    $('.talk > img').show();
-  });
-});
-
-// 이하동문
-$(document).ready(function () {
-  $('.partnership').mouseover(function () {
-    $('.partnership > p').show();
-    $('.partnership > img').hide();
-  });
-  $('.partnership').mouseout(function () {
-    $('.partnership > p').hide();
-    $('.partnership > img').show();
-  });
-
-  $('.memo').mouseover(function () {
-    $('.memo > p').show();
-    $('.memo > img').hide();
-  });
-  $('.memo').mouseout(function () {
-    $('.memo > p').hide();
-    $('.memo > img').show();
-  });
-});
+      // 검색창 초기화 스크립트
+    window.addEventListener('pageshow', function (event) {
+    if (event.persisted) {
+        const searchInput = document.querySelector('input[name="query"]');
+        if (searchInput) searchInput.value = '';
+    }
+    });
